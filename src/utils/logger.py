ed4e815e4604d13b84da877154a6c5f6f59c378c
troubleShooter239 <morgun2282@gmail.com>
2025-02-18 13:55:16 +0300

    add logs dir

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,6 +1,8 @@
 from logging import getLogger, Formatter, INFO, ERROR, StreamHandler
 from logging.handlers import RotatingFileHandler
 from sys import stdout
+from os.path import exists
+from os import makedirs
 
 logger = getLogger("face-analyze-backend")
 logger.setLevel(INFO)
@@ -12,7 +14,10 @@ console_handler.setLevel(INFO)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
-file_handler = RotatingFileHandler("app.log", maxBytes=10 ** 6, backupCount=5)
+logs_dir = 'logs'
+if not exists(logs_dir):
+    makedirs(logs_dir)
+file_handler = RotatingFileHandler(f"{logs_dir}/app.log", maxBytes=10 ** 6, backupCount=5)
 file_handler.setLevel(ERROR)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
